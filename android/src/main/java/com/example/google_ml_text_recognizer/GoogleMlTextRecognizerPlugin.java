c1233d507aa8d2dc0b152cfdf1dc1908f34b689e
daniel.jonathan <daniel.jonathan@smartfren.com>
2021-10-06 10:55:10 +0700

    Added TextDetector implementation for android Added example implementation for android

--- android/src/main/java/com/example/google_ml_text_recognizer/GoogleMlTextRecognizerPlugin.java
+++ android/src/main/java/com/example/google_ml_text_recognizer/GoogleMlTextRecognizerPlugin.java
@@ -5,11 +5,10 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 /** GoogleMlTextRecognizerPlugin */
-public class GoogleMlTextRecognizerPlugin implements FlutterPlugin, MethodCallHandler {
+public class GoogleMlTextRecognizerPlugin implements FlutterPlugin {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
@@ -19,16 +18,7 @@ public class GoogleMlTextRecognizerPlugin implements FlutterPlugin, MethodCallHa
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "google_ml_text_recognizer");
-    channel.setMethodCallHandler(this);
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
-    }
+    channel.setMethodCallHandler(new MlTextMethodCallHandler(flutterPluginBinding.getApplicationContext()));
   }
 
   @Override
--- lib/google_ml_text_recognizer.dart
+++ lib/google_ml_text_recognizer.dart
@@ -1,15 +1,1 @@
-// import 'dart:async';
-
-// import 'package:flutter/services.dart';
-
-// class GoogleMlTextRecognizer {
-//   static const MethodChannel _channel =
-//       const MethodChannel('google_ml_text_recognizer');
-
-//   static Future<String?> get platformVersion async {
-//     final String? version = await _channel.invokeMethod('getPlatformVersion');
-//     return version;
-//   }
-// }
-
 export 'src/google_ml_vision.dart';
